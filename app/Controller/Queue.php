eacdb17dc022adcff65a1ca6222fbc86bac96fbc
lord_MATATA <s.mtz.hakim@gmail.com>
2020-09-20 16:57:59 +0430

    some how finish the get_mesage_threrade in controler Queue

--- app/Controller/Queue.php
+++ app/Controller/Queue.php
@@ -6,6 +6,8 @@ use App\Model\Queues;
 
 use MangaCrawlers\Validator;
 
+use function PHPSTORM_META\type;
+
 class Queue
 {
     private $error = [];
@@ -14,20 +16,23 @@ class Queue
     {
         $msg = new Messages();
         $Q = new Queues();
-        if (
-            !$Q->set_queue(
+        $start_chapter = $msg->get_all_messages($_chat_id)[2]['content'];
+        $finish_chapter = $msg->get_all_messages($_chat_id)[3]['content'];
+
+        // later on add the > if(type == 1) to handel normal and vip
+
+        for ($i = $start_chapter; $i <= $finish_chapter; $i++) {
+            $Q->set_queue(
                 $_chat_id,
-                $msg->get_all_messages($_chat_id)[0],
-                $msg->get_all_messages($_chat_id)[1],
-                $msg->get_all_messages($_chat_id)[2],
+                $msg->get_all_messages($_chat_id)[0]['content'],
+                $msg->get_all_messages($_chat_id)[1]['content'],
+                $i,
                 1,
                 time(),
                 "pendindg"
-            )
-        ) {
-            $this->error["message"] = "couldnt set queue";
-            return false;
+            );
         }
+
         if (!$msg->finish($_chat_id)) {
             $this->error["message"] = "couldnt erase the message from database";
             return false;
