9ddb3f18c09a0b6a55977d259eda43413c2f8d96
lord_MATATA <s.mtz.hakim@gmail.com>
2020-09-28 19:47:20 +0330

    fix again

--- app/Controller/Message.php
+++ app/Controller/Message.php
@@ -14,6 +14,7 @@ class Message
     private $request;
     private $db;
     private $usr;
+    private $user_vip;
     private $tg;
     private $Q;
     private $arr = ["start" => "/start", "help" => "/help"];
