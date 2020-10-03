26e1875aed8be626e38b165586e57f836d19d973
lord_MATATA <s.mtz.hakim@gmail.com>
2020-10-03 14:49:47 +0330

    fix the vip queue

--- app/Controller/Queue.php
+++ app/Controller/Queue.php
@@ -78,10 +78,6 @@ class Queue
                         $this->error["message"] = "couldnt do set_queue properly";
                         return false;
                     }
-                    if (!$msg->finish($_chat_id)) {
-                        $this->error["message"] = "couldnt erase the message from database";
-                        return false;
-                    }
                 }
                 $this->error["message"] = "couldnt do send_file_id_request properly";
                 return false;
