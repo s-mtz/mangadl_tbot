b484e23884289896f2fb79b4243cb15aa14b0b27
lord_MATATA <s.mtz.hakim@gmail.com>
2020-10-18 12:13:06 +0330

    fix the querry

--- app/Model/User.php
+++ app/Model/User.php
@@ -19,7 +19,7 @@ class User extends ModelAbstract
      */
     public function new_user(string $_chat_id, int $_time)
     {
-        $sql = "INSERT INTO user (chat_id, time) 
+        $sql = "INSERT INTO user (chat_id, `time`) 
         VALUES ('{$_chat_id}', $_time)";
 
         if ($this->conn->query($sql) === false) {
